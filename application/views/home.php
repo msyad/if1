7e841da28c2bca3691d5a11e23611a39f82870d3
irsyad <mr.seicho007@gmail.com>
2017-03-16 01:35:15 +0800

    fery dewa

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$title?></title>
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css">
+	<script src="<?=base_url()?>js/jquery-1.12.4.js" type="text/javascript"></script>
+	<script src="<?=base_url()?>js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="container">
+		<h3>Hello World</h3>
+	</div>
+</body>
+</html>
