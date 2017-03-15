42285b1b58e2962f855bb2f5bac025643aff8b6b
irsyad <mr.seicho007@gmail.com>
2017-03-16 04:43:19 +0800

    adsfsa

--- application/views/home.php
+++ application/views/home.php
@@ -6,11 +6,28 @@
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css">
 	<script src="<?=base_url()?>js/jquery-1.12.4.js" type="text/javascript"></script>
 	<script src="<?=base_url()?>js/bootstrap.min.js" type="text/javascript"></script>
+	<style type="text/css" media="screen">
+		input {
+			max-width: 250px;
+		}	
+	</style>
 </head>
 <body>
 	<div id="container">
 		<h3>Hello World</h3>
 		Grandong
+		<div class="panel-default">
+			<div class="panel-heading">
+				<h3>Panel Grandong</h3>
+			</div>
+			<div class="panel-body">
+				<form id="form1" accept-charset="utf-8">
+					<input type="text" name="usr" id="usr" placeholder="Username" class="form-control"><br>
+					<input type="password" name="pwd" id="pwd" placeholder="Password" class="form-control">
+
+				</form>
+			</div>
+		</div>
 	</div>
 </body>
 </html> 
